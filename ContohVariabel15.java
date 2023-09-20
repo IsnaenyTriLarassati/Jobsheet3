1941196f8222b492fbca7b227eb9771dd5a8980f
isnaenytrilarassati <isnaenytrilarassati>
2023-09-20 11:38:16 +0700

    Percobaan1 20-09-23

---
+++ ContohVariabel15.java
@@ -0,0 +1,22 @@
+package Jobsheet3;
+
+
+/**
+ * ContohVariabel15
+ */
+public class ContohVariabel15 {
+
+    public static void main(String[] args) {
+        String salahSatuHobiSayaAdalah = "Menonton film";
+        boolean isPandai =true;
+        char JenisKelamin = 'P';
+        byte _umurSayaSekarang = 19;
+        double $ipk = 3.5, tinggi = 150;
+
+        System.out.println(salahSatuHobiSayaAdalah);
+        System.out.println("Apakah pandai? " + isPandai);
+        System.out.println("Jenis kelamin: " + JenisKelamin);
+        System.out.println("IPK: " + $ipk);
+        System.out.println(String.format("Saya berumur %s dengan tinggi badan %s", _umurSayaSekarang, tinggi));
+    }
+}
